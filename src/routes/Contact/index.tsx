f8420f4b5629b470a5c58172eae13021e4e6c683
wkqkel <89131236+wkqkel@users.noreply.github.com>
2022-06-11 05:47:24 +0900

    refactor: Contact페이지 sendEmail함수 분리

--- src/routes/Contact/index.tsx
+++ src/routes/Contact/index.tsx
@@ -1,5 +1,6 @@
-import { FormEvent, useState } from 'react';
-import emailjs from '@emailjs/browser';
+import { FormEvent, useCallback, useState } from 'react';
+
+import { sendEmail } from './utils';
 
 import { ArrowSendIcon } from 'assets/svgs';
 
@@ -8,35 +9,17 @@ import styles from './contact.module.scss';
 const Contact = () => {
   const [resultText, setResultText] = useState('');
 
-  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+  const onSubmit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        `${process.env.REACT_APP_EMAILJS_SERVICE_KEY}`,
-        `${process.env.REACT_APP_EMAILJS_TEMPLATE_KEY}`,
-        e.currentTarget,
-        `${process.env.REACT_APP_EMAILJS_PUBLIC_KEY}`
-      )
-      .then(
-        (result) => {
-          if (result.text === 'OK') {
-            setResultText('Thanks, I will contact you soon.');
-          }
-        },
-        (error) => {
-          if (error) {
-            setResultText('Sorry, You failed to send.');
-          }
-        }
-      );
-  };
+    setResultText(await sendEmail(e.currentTarget));
+  }, []);
 
   return (
     <section className={styles.contact}>
       {resultText && <div className={styles.result}>{resultText}</div>}
       <div>
-        <form onSubmit={sendEmail} className={styles.form}>
+        <form onSubmit={onSubmit} className={styles.form}>
           <label htmlFor='user_name'>Your Name</label>
           <input type='text' name='user_name' autoComplete='off' required />
           <label htmlFor='user_email'>Your Email Address</label>
